03a08723dbc0d59f783e56533194f9f30a1a48c2
Minos Galanakis <minos197@gmail.com>
2016-09-13 10:38:36 +0100

    [picrypt.c]: Layout changes, added ifdef so that portion of the codes are omitted when not defined, fixed a bug in machine-id comparison, added new information to vhash, whitespace changes

--- picrypt.c
+++ picrypt.c
@@ -41,8 +41,7 @@ char * sha1_from_file(char * fname, char *sha_hash)
 
   // Open the file
   fptr=fopen("/etc/fstab", "rb");
-  if (!fptr)
-  {
+  if (!fptr) {
     fprintf(stderr, "Unable to open file %s", "name");
     return NULL;
   }
@@ -64,7 +63,7 @@ char * sha1_from_file(char * fname, char *sha_hash)
 
   uint8_t j=0;
   /* Go through the buffer, convert the chars to zero-padded hex strings */
-  for(int i = 0;i < SHA_DIGEST_LENGTH;++i){
+  for(int i = 0;i < SHA_DIGEST_LENGTH;++i) {
     snprintf(sha_hash+j, 4 ,"%02x", sha1_buffer[i]);
     j += 2;
   }
@@ -73,6 +72,7 @@ char * sha1_from_file(char * fname, char *sha_hash)
   return sha_hash;
 }
 
+#if defined(FILE_SEED) && defined(FILE_SHA1)
 char * sha1_from_en_buf(int16_t const *fname,const uint16_t byte_size, char *sha_hash)
 {
   /* Get the file path */
@@ -93,81 +93,82 @@ int16_t * sha1_from_en_buf_to_en_buff(int16_t const *fname,
   encrypt_string(sha_d, sha_buff, SHA_DIGEST_LENGTH*4);
   return sha_buff;
 }
+#endif
 
 void string_slice_from_file(char const *fname,
                             const uint32_t offset,
                             const uint32_t byte_size,
                             char *buffer)
 {
-        FILE *fptr;
-        // Open the file
-        fptr=fopen(fname, "r");
-        if(fptr==NULL) {
-                printf("Error!");
-                exit(1);
-        }
-        if (offset) {
-                // Go to offset where the serial number is stored
-                fseek(fptr, offset, SEEK_SET);
-        }
-        /* Read the data from the file into the string */
-        fgets(buffer, byte_size+1, fptr);
-        fclose(fptr);
-        return;
+  FILE *fptr;
+  // Open the file
+  fptr=fopen(fname, "r");
+  if(fptr==NULL) {
+    printf("Error Opening File!");
+    exit(1);
+  }
+  if (offset) {
+    // Go to offset where the serial number is stored
+    fseek(fptr, offset, SEEK_SET);
+  }
+  /* Read the data from the file into the string */
+  fgets(buffer, byte_size+1, fptr);
+  fclose(fptr);
+  return;
 }
 
 uint64_t pi_serial()
 {
-
-        char ser_buffer[CPU_SER_SIZE+1];
-        string_slice_from_file(CPU_FILE, CPU_SER_OFFSET, CPU_SER_SIZE, ser_buffer);
-        return (uint64_t)strtoull(ser_buffer, NULL, 16);
+  char ser_buffer[CPU_SER_SIZE+1];
+  string_slice_from_file(CPU_FILE, CPU_SER_OFFSET, CPU_SER_SIZE, ser_buffer);
+  return (uint64_t)strtoull(ser_buffer, NULL, 16);
 }
 
 void soft_machine_id(char *ret_buff)
 {
-        string_slice_from_file(MACHINE_ID_FILE, 0, MACHINE_ID_SIZE, ret_buff);
-        return;
+  string_slice_from_file(MACHINE_ID_FILE, 0, MACHINE_ID_SIZE, ret_buff);
+  return;
 }
 
 void ram_key(const uint64_t hash_key)
 {
-        FILE *fptr;
-        fptr=fopen(RAM_FILE,"w");
-        if(fptr==NULL) {
-                printf("Error!");
-                exit(1);
-        }
+  FILE *fptr;
+  fptr=fopen(RAM_FILE,"w");
+  if(fptr==NULL) {
+    printf("Error!");
+    exit(1);
+  }
 
-        char key_text[28];
-        sprintf(key_text, "passphrase_passwd=%" PRIx64 "", hash_key);
-        fprintf(fptr,"%s\n",key_text);
-        fclose(fptr);
+  char key_text[28];
+  sprintf(key_text, "passphrase_passwd=%" PRIx64 "", hash_key);
+  fprintf(fptr,"%s\n",key_text);
+  fclose(fptr);
 }
 
 bool validate_key(char const *key)
 {
-        if (strlen(key) > 8)
-        {
-                return false;
-        }
-        uint64_t ukey = (uint64_t)strtoull(key, NULL, 16);
-        uint64_t hdw_key = hash(pi_serial());
+  if (strlen(key) > 8) {
+    return false;
+  }
+  uint64_t ukey = (uint64_t)strtoull(key, NULL, 16);
+  uint64_t hdw_key = hash(pi_serial());
 
-        if (ukey == hdw_key) {
-                return true;
-        } else {
-                return false;
-        }
+  if (ukey == hdw_key) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void help(const char* prgm)
 {
-        printf("<---------------------- Options -------------------------------->\n");
-        printf("%s --hash          : Generate Unique Machine Hash\n", prgm);
-        printf("%s --vhash         : Print Verbose Unique Machine Hash\n", prgm);
-        printf("%s --ramkey        : Create an encryptfs unlock key to RAM \n", prgm);
-        printf("%s --check xxxxxx  : Check if a key is valid or not \n", prgm);
+  printf("<---------------------- Options -------------------------------->\n");
+  #ifdef HWD_ID
+  printf("%s --hash          : Generate Unique Machine Hash\n", prgm);
+  printf("%s --ramkey        : Create an encryptfs unlock key to RAM \n", prgm);
+  printf("%s --check xxxxxx  : Check if a key is valid or not \n", prgm);
+  #endif
+  printf("%s --vhash         : Print Verbose Unique Machine Hash\n", prgm);
 }
 
 /********************
@@ -176,111 +177,121 @@ void help(const char* prgm)
 
 int main(int argc, char **argv)
 {
-        bool permitted = true;
-        #ifdef HWD_ID
-          uint64_t serial = pi_serial();
-          uint64_t hash_key = hash(serial);
-          uint64_t permitted_serial = (uint64_t)strtoull(HWD_ID, NULL, 16);
-          if ((PROTECTION >= SCRIPT_KIDDY) && (permitted_serial != serial))
-          {
-            printf("Serial Miss-Match!! \n");
-            permitted = false;
-          }
-        #endif
-        #ifdef MACHINE_ID
-          /* Allocate buffers */
-          int16_t machine_id_hd_e[MACHINE_ID_SIZE * 2];
-          int16_t machine_id_rt_e[MACHINE_ID_SIZE * 2];
+  bool permitted = true;
+  #ifdef HWD_ID
+  uint64_t serial = pi_serial();
+  uint64_t hash_key = hash(serial);
+  uint64_t permitted_serial = (uint64_t)strtoull(HWD_ID, NULL, 16);
+  if ((PROTECTION >= SCRIPT_KIDDY) && (permitted_serial != serial)) {
+    printf("Serial Miss-Match!! \n");
+    permitted = false;
+  }
+  #endif
+  #ifdef MACHINE_ID
+  /* Allocate buffers */
+  int16_t machine_id_hd_e[MACHINE_ID_SIZE];
+  int16_t machine_id_rt_e[MACHINE_ID_SIZE];
 
-          /* Exctact Current Machine ID (not sensitive) */
-          char machine_id_hd_d[MACHINE_ID_SIZE + 1];
-          soft_machine_id(machine_id_hd_d);
+  /* Exctact Current Machine ID (not sensitive) */
+  char machine_id_hd_d[MACHINE_ID_SIZE + 1];
+  soft_machine_id(machine_id_hd_d);
 
-          encrypt_string(MACHINE_ID, machine_id_hd_e, sizeof(machine_id_hd_e));
-          encrypt_string(machine_id_hd_d, machine_id_rt_e, sizeof(machine_id_rt_e));
+  encrypt_string(MACHINE_ID, machine_id_hd_e, sizeof(machine_id_hd_e));
+  encrypt_string(machine_id_hd_d, machine_id_rt_e, sizeof(machine_id_rt_e));
 
-          if (PROTECTION >= ARCH_USER)
-          {
-            if (!compare_encrypted_str(machine_id_rt_e,
-                              machine_id_hd_e,
-                              sizeof(machine_id_rt_e),
-                              sizeof(machine_id_hd_e)))
-            {
-              printf("Machine ID Miss Match!! \n");
-              permitted = false;
-            }
-          }
-        #endif
+  if (PROTECTION >= ARCH_USER) {
+    if (!compare_encrypted_str(machine_id_rt_e,
+                               machine_id_hd_e,
+                               sizeof(machine_id_rt_e),
+                               sizeof(machine_id_hd_e))) {
+      printf("Machine ID Miss Match!! \n");
+      permitted = false;
+    }
+  }
+  #endif
 
-        #if defined(FILE_SEED) && defined(FILE_SHA1)
-          /* Allocate the buffers that will hide the filename and seed */
-          /* Naming convention:
-           variable_hd/rt_e -> variable name,
-                               hash_defined/runtime
-                               encrypted/decrypted*/
-          int16_t file_seed_hd_e[strlen(FILE_SEED)];
-          int16_t file_sha_hd_e[SHA_DIGEST_LENGTH * 2];
-          int16_t file_sha_rt_e[SHA_DIGEST_LENGTH * 2];
+  #if defined(FILE_SEED) && defined(FILE_SHA1)
+  /* Allocate the buffers that will hide the filename and seed */
+  /* Naming convention:
+   variable_hd/rt_e -> variable name,
+                       hash_defined/runtime
+                       encrypted/decrypted*/
+  int16_t file_seed_hd_e[strlen(FILE_SEED)];
+  int16_t file_sha_hd_e[SHA_DIGEST_LENGTH * 2];
+  int16_t file_sha_rt_e[SHA_DIGEST_LENGTH * 2];
 
-          /* Allocate the memory for the decryption buffers */
-          char file_sha_rt_d[(SHA_DIGEST_LENGTH * 2) + 1];
-          char sha_hash_rt_d[(SHA_DIGEST_LENGTH * 2) + 1];
+  /* Allocate the memory for the decryption buffers */
+  // char file_sha_rt_d[(SHA_DIGEST_LENGTH * 2) + 1]; /* Placeholder */
+  char sha_hash_rt_d[(SHA_DIGEST_LENGTH * 2) + 1];
+  char file_seed_hd_d[strlen(FILE_SEED)+1];
 
-          /* Store the sensitive information */
-          encrypt_string(FILE_SEED, file_seed_hd_e, sizeof(file_seed_hd_e));
-          encrypt_string(FILE_SHA1, file_sha_hd_e, sizeof(file_sha_hd_e));
-          sha1_from_en_buf_to_en_buff(file_seed_hd_e, sizeof(file_seed_hd_e), file_sha_rt_e);
+  /* Store the sensitive information */
+  encrypt_string(FILE_SEED, file_seed_hd_e, sizeof(file_seed_hd_e));
+  encrypt_string(FILE_SHA1, file_sha_hd_e, sizeof(file_sha_hd_e));
+  sha1_from_en_buf_to_en_buff(file_seed_hd_e,
+                              sizeof(file_seed_hd_e),
+                              file_sha_rt_e);
 
-          printf("Defined SHA: %s\n", decrypt_string(file_sha_hd_e, file_sha_rt_d, sizeof(file_sha_rt_d)) );
-          printf("Runtime SHA: %s\n", decrypt_string(file_sha_rt_e, sha_hash_rt_d, sizeof(sha_hash_rt_d)));
+  if (PROTECTION >= PEN_TESTER) {
+    if (!compare_encrypted_str(file_sha_rt_e,
+                               file_sha_hd_e,
+                               sizeof(file_sha_rt_e),
+                               sizeof(file_sha_hd_e))) {
+      printf("SHA1 Miss-Match!! \n");
+      permitted = false;
+    }
+  }
+  #endif
 
-          if (PROTECTION >= PEN_TESTER)
-          {
-            if (!compare_encrypted_str(file_sha_rt_e,
-                            file_sha_hd_e,
-                            sizeof(file_sha_rt_e),
-                            sizeof(file_sha_hd_e)))
-            {
-              printf("SHA1 Miss-Match!! \n");
-              permitted = false;
-            }
-          }
+  /* Ensure the executable is run from intended machine.
+     This check is important because the executable is not encrypted,
+     and running it over to a different hwd would be an attack vector. */
+  if (!permitted) {
+    printf("This is the password you are looking for...\n"
+           "( You pesky pirate !!! )\n");
+    exit(1);
+  }
 
-
-        #endif
-        /* Ensure the executable is run from intended machine.
-           This check is important because the executable is not encrypted,
-           and running it over to a different hwd would be an attack vector. */
-
-        if (!permitted)
-        {
-          printf("This is the password you are looking for...\n"
-                 "( You pesky pirate !!! )\n");
-          exit(1);
-        }
-
-        if (argc == 1) {
-                help(argv[0]);
-        } else if (argc == 2 && !strcmp(argv[1],"--ramkey")) {
-                ram_key(hash(serial));
-                printf("%" PRIx64 "\n", hash_key);
-        } else if (argc == 2 && !strcmp(argv[1],"--vhash")) {
-                printf("Calculating Hash\n");
-                printf("Serial (int): %" PRIu64 "\nSerial (hex): %" PRIx64 " \n", serial, serial);
-                printf("Machine-id:   %s\n",machine_id_hd_d);
-                printf("Hash Key:     %" PRIx64 "\n", hash_key);
-        } else if (argc == 2 && !strcmp(argv[1],"--hash")) {
-                printf("%" PRIx64 "\n", hash_key);
-        } else if (argc == 3 && !strcmp(argv[1],"--check")) {
-                if (validate_key(argv[2])) {
-                        printf("Key %s is Valid\n", argv[2]);
-                        return 0;
-                } else {
-                        printf("Key %s is Invalid\n", argv[2]);
-                        return 1;
-                }
-        } else {
-                help(argv[0]);
-        }
-        return 0;
+  if (argc == 1) {
+    help(argv[0]);
+  #ifdef HWD_ID
+  } else if (argc == 2 && !strcmp(argv[1],"--ramkey")) {
+    ram_key(hash(serial));
+    printf("%" PRIx64 "\n", hash_key);
+  } else if (argc == 2 && !strcmp(argv[1],"--hash")) {
+    printf("%" PRIx64 "\n", hash_key);
+  } else if (argc == 3 && !strcmp(argv[1],"--check")) {
+    if (validate_key(argv[2])) {
+      printf("Key %s is Valid\n", argv[2]);
+      return 0;
+    } else {
+      printf("Key %s is Invalid\n", argv[2]);
+      return 1;
+    }
+  #endif
+  } else if (argc == 2 && !strcmp(argv[1],"--vhash")) {
+    printf("\n[ Runtime Hardware Keys ] \n");
+    #ifdef HWD_ID
+    printf("Serial (i): %" PRIu64 "\nSerial (0x): %" PRIx64 " \n",
+           serial,
+           serial);
+    printf("Hash Key:     %" PRIx64 "\n", hash_key);
+    #endif
+    #ifdef MACHINE_ID
+    printf("Machine-id:   %s\n",machine_id_hd_d);
+    #endif
+    #if defined(FILE_SEED) && defined(FILE_SHA1)
+    printf("KeyFile:      %s\n",
+            decrypt_string(file_seed_hd_e,
+                           file_seed_hd_d,
+                           sizeof(file_seed_hd_d)));
+    printf("SHA1 Key:     %s\n",
+           decrypt_string(file_sha_rt_e,
+                          sha_hash_rt_d,
+                          sizeof(sha_hash_rt_d)));
+    #endif
+  } else {
+    help(argv[0]);
+  }
+  return 0;
 }
