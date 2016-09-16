d94653dbee82ba50987302bd8e2a52b3d5f7a044
Minos Galanakis <minos197@gmail.com>
2016-09-16 15:19:18 +0100

    [ppprocessor.c]: Added new pre-pre-processor file

---
+++ ppprocessor.c
@@ -0,0 +1,20 @@
+/*****************************************************************************\
+ * PiCrypt is a software key generator for ecryptfs locked system.            *
+ * Since the program is meant to unlock the protected area, it has to be      *
+ * decrypted itself but will perform a set of  hardware tests before producing*
+ * the unlock key.                                                            *
+ *                                                                            *
+ * Author: Minos Galanakis                                                    *
+ * Email: minos197@gmail.com                                                  *
+ * License: GPL v3.0                                                          *
+ * Project: PiCrypt                                                           *
+ * File Description: Method Implementations                                   *
+ \****************************************************************************/
+#include "authorized_hwd.h"
+#include "strhide.h"
+
+
+int main(int argc, const char *argv[])
+{
+  parse_header("authorized_hwd.h", "authorized_hwd_e.h");
+}
